aabceeab99ee8468e969ac083c57af877deceb9b
Omar Ehab <oe1001@keemail.me>
2023-05-11 18:10:03 +0300

    Added verificationToken, isVerified, verified properties in User model

--- models/User.js
+++ models/User.js
@@ -28,6 +28,12 @@ const userSchema = new mongoose.Schema({
     enum: ["admin", "user"],
     default: "user",
   },
+  verificationToken: String,
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  verified: Date,
 });
 
 // Encryting Passwords before the document is saved
